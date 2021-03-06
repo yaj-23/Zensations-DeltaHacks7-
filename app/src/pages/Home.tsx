9fc59ae8486da6762df0e53b39a81920dfbd1bf8
Aryan Patel <69769265+AryPat@users.noreply.github.com>
2021-03-06 01:28:07 -0500

    Removed Initial Ionic Formatting

--- app/src/pages/Home.tsx
+++ app/src/pages/Home.tsx
@@ -1,22 +1,11 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
 import './Home.css';
 
 const Home: React.FC = () => {
   return (
     <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Blank</IonTitle>
-        </IonToolbar>
-      </IonHeader>
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
+        <div>STUFF GOES HERE</div>
       </IonContent>
     </IonPage>
   );
