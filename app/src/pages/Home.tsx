ba43c3a602f1df9e7c4df73d92eb673494710d5e
Yajurva Trivedi <yajurva.trivedi@ryerson.ca>
2021-03-06 02:22:36 -0500

    idk

--- app/src/pages/Home.tsx
+++ app/src/pages/Home.tsx
@@ -1,20 +1,36 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import './Home.css';
 import lotus from '../logos/lotus.svg';
+import icon from '../logos/frameIcon.svg';
+import getStartedBtn from '../logos/getStarted_Btn.svg';
+import learnmoreBtn from '../logos/learnMore_Btn.svg';
 
 const Home: React.FC = () => {
   return (
     <IonPage>
-      <IonContent fullscreen>
+
+        
         <div className="mainScreen">
+          
+
+          <div className='icon'>
+            <img className="green-background" src={icon}/>
+          </div>
+
           <div className="title">
             <div>Zensation</div>
             <div>release your emotions</div>
             
           </div>
           <img src={lotus} alt="lotus" />
+          <div className='btns'>
+            <img className='getStartedbtn' src={getStartedBtn} alt="getStartedBtn" />
+            <img src={learnmoreBtn} alt="learnmoreBtn" />
+          </div>
+         
+
         </div>
-      </IonContent>
+     
     </IonPage>
   );
 };
