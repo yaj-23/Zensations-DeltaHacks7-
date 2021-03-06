846d49fc9113f3f902ae9bfe9e9ba5e44112f0ba
Samyakk123 <samyak.mehta@mail.utoronto.ca>
2021-03-06 02:40:10 -0500

    Modified index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,9 @@
-import express, { Request, Response } from 'express';
-import cors from 'cors';
-import dotenv from 'dotenv';
-import { PORT } from './config/constants';
-import { quizRouter } from './routes';
+import express, { Request, Response } from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import { PORT, COCKROACH_CONFIG } from "./config/constants";
+import { quizRouter } from "./routes";
+import { Pool } from "pg";
 
 dotenv.config();
 
@@ -13,12 +14,21 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use('/api/quiz', quizRouter);
+app.use("/api/quiz", quizRouter);
 
-app.get('/', (req: Request, res: Response) => {
-    res.send('IF YOU SEE THIS, THE SERVER IS WORKING');
+app.get("/", (req: Request, res: Response) => {
+  res.send("IF YOU SEE THIS, THE SERVER IS WORKING");
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT}`);
+  console.log(`Server is listening on port ${PORT}`);
 });
+
+// const pool = new Pool(COCKROACH_CONFIG);
+
+(async () => {
+  //   const client = await pool.connect();
+
+  // Exit program
+  process.exit();
+})().catch((err) => console.log(err.stack));
