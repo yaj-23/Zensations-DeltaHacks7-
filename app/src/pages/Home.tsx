1bbdc1633eacb1afc95a099612b636fdaf1d4fff
Aryan Patel <69769265+AryPat@users.noreply.github.com>
2021-03-06 03:08:03 -0500

    Updated Font

--- app/src/pages/Home.tsx
+++ app/src/pages/Home.tsx
@@ -1,36 +1,44 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import './Home.css';
-import lotus from '../logos/lotus.svg';
-import icon from '../logos/frameIcon.svg';
-import getStartedBtn from '../logos/getStarted_Btn.svg';
-import learnmoreBtn from '../logos/learnMore_Btn.svg';
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/react";
+import "./Home.css";
+import lotus from "../logos/lotus.svg";
+import icon from "../logos/frameIcon.svg";
+import getStartedBtn from "../logos/getStarted_Btn.svg";
+import learnmoreBtn from "../logos/learnMore_Btn.svg";
 
 const Home: React.FC = () => {
   return (
     <IonPage>
-
-        
-        <div className="mainScreen">
-          
-
-          <div className='icon'>
-            <img className="green-background" src={icon}/>
-          </div>
-
-          <div className="title">
-            <div className="app-title">Zensation</div>
-            <div className="app-slogan">release your emotions</div>
-            
-          </div>
-          <img src={lotus} alt="lotus" />
-          <div className='btns'>
-            <img  className='button-background' src={getStartedBtn} alt="getStartedBtn" onClick={()=> console.log("MOVE ME TO NEXT PAGE")} />
-            <img className='button-background' src={learnmoreBtn} alt="learnmoreBtn" onClick={()=> console.log("MOVE ME TO NEXT PAGE")} />
-          </div>
-         
-
+      <div className="mainScreen">
+        <div className="icon">
+          <img className="green-background" src={icon} />
         </div>
-     
+
+        <div className="title">
+          <div className="app-title">Zensation</div>
+          <div className="app-slogan">release your emotions</div>
+        </div>
+        <img src={lotus} alt="lotus" />
+        <div className="btns">
+          <img
+            className="button-background"
+            src={getStartedBtn}
+            alt="getStartedBtn"
+            onClick={() => console.log("MOVE ME TO NEXT PAGE")}
+          />
+          <img
+            className="button-background"
+            src={learnmoreBtn}
+            alt="learnmoreBtn"
+            onClick={() => console.log("MOVE ME TO NEXT PAGE")}
+          />
+        </div>
+      </div>
     </IonPage>
   );
 };
