ed26d476bdf102352a69f0b58e9f1f35e9129ad6
Aryan Patel <69769265+AryPat@users.noreply.github.com>
2021-03-06 01:46:22 -0500

    Loading Page

--- app/src/pages/Home.tsx
+++ app/src/pages/Home.tsx
@@ -1,11 +1,19 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import './Home.css';
+import lotus from '../logos/lotus.svg';
 
 const Home: React.FC = () => {
   return (
     <IonPage>
       <IonContent fullscreen>
-        <div>STUFF GOES HERE</div>
+        <div className="mainScreen">
+          <div className="title">
+            <div>Zensation</div>
+            <div>release your emotions</div>
+            
+          </div>
+          <img src={lotus} alt="lotus" />
+        </div>
       </IonContent>
     </IonPage>
   );
