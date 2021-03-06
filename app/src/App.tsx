ad3a1977e99fcc44369d529158e05d92306ebd18
Aryan Patel <69769265+AryPat@users.noreply.github.com>
2021-03-06 03:13:53 -0500

    me no see

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -2,6 +2,7 @@ import { Redirect, Route } from 'react-router-dom';
 import { IonApp, IonRouterOutlet } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
 import Home from './pages/Home';
+import Question1 from './pages/Question1';
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
@@ -27,7 +28,7 @@ const App: React.FC = () => (
     <IonReactRouter>
       <IonRouterOutlet>
         <Route exact path="/home">
-          <Home />
+          <Question1 />
         </Route>
         <Route exact path="/">
           <Redirect to="/home" />
