4cd0244a6ce2f93700bb0d73b781355dc8b549a1
Navinn Ravindaran <59669957+navn-r@users.noreply.github.com>
2021-03-06 02:26:13 -0500

    updated cockroach config

--- server/src/config/constants.ts
+++ server/src/config/constants.ts
@@ -1,2 +1,12 @@
 export const PORT = process.env.PORT || 4000;
 export const COCKROACH_URL = process.env.COCKROACH_URL || "";
+export const COCKROACH_CONFIG = {
+  user: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
+  host: process.env.PGHOST,
+  database: process.env.PGDATABASE,
+  port: process.env.PGPORT,
+  ssl: {
+    rejectUnauthorized: false,
+  }
+};
