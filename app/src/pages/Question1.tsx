041236b3cef5f3f58c9b1ed9d3b37bdfbb21325a
Aryan Patel <69769265+AryPat@users.noreply.github.com>
2021-03-06 12:51:56 -0500

    Cleaner Code

--- app/src/pages/Question1.tsx
+++ app/src/pages/Question1.tsx
@@ -115,88 +115,32 @@ const Question1: React.FC = () => {
     );
   } else if (count == 2) {
     emojiDisplay = (
-      <div>
-        <div className="emoji-icon-row1">
-          <img
-            className="emoji-btn"
-            src={sad}
-            alt="nextBtn"
-            onClick={() => addHandler(1)}
-          />
-          <img
-            className="emoji-btn"
-            src={crying}
-            alt="nextBtn"
-            onClick={() => addHandler(0)}
-          />
-          <img
-            className="emoji-btn"
-            src={confused}
-            alt="nextBtn"
-            onClick={() => addHandler(6)}
-          />
-        </div>
-
-        <div className="emoji-icon-row2">
-          <img
-            className="emoji-btn"
-            src={angry}
-            alt="nextBtn"
-            onClick={() => addHandler(3)}
-          />
-          <img
-            className="emoji-btn"
-            src={happy}
-            alt="nextBtn"
-            onClick={() => addHandler(4)}
-          />
-          <img
-            className="emoji-btn"
-            src={board}
-            alt="nextBtn"
-            onClick={() => addHandler(7)}
-          />
-        </div>
+      <div className="emoji-rows">
+        {[sad, crying, confused, angry, happy, board].map(
+          (emoji: any, index: number) => (
+            <img
+              className="emoji-btn"
+              key={index}
+              src={emoji}
+              alt="nextBtn"
+              onClick={() => addHandler(index)}
+            />
+          )
+        )}
       </div>
     );
   } else {
     emojiDisplay = (
-      <div>
-        <div className="emoji-icon-row1">
+      <div className="emoji-rows">
+        {[one, two, three, four, five, six].map((emoji: any, index: number) => (
           <img
             className="emoji-btn"
-            src={one}
+            key={index}
+            src={emoji}
             alt="nextBtn"
-            onClick={() => addHandler(1)}
+            onClick={() => addHandler(index + 1)}
           />
-          <img
-            className="emoji-btn"
-            src={two}
-            alt="nextBtn"
-            onClick={() => addHandler(2)}
-          />
-          <img
-            className="emoji-btn"
-            src={three}
-            alt="nextBtn"
-            onClick={() => addHandler(3)}
-          />
-        </div>
-
-        <div className="emoji-icon-row2">
-          <img
-            className="emoji-btn"
-            src={four}
-            alt="nextBtn"
-            onClick={() => addHandler(4)}
-          />
-          <img
-            className="emoji-btn"
-            src={five}
-            alt="nextBtn"
-            onClick={() => addHandler(5)}
-          />
-        </div>
+        ))}
       </div>
     );
   }
